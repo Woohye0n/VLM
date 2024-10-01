496c947e8023ef030ea53ac978b64c2916d5f664
Woohye0n <wh761@g.skku.edu>
2024-10-01 22:37:06 +0900

    10/01 코드정리

--- main.py
+++ main.py
@@ -77,17 +77,16 @@ for idx, i in enumerate(range(len(dataset))):
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
         
         # Merge to semantic mask
-        get_value = mask_dict.get(category_name)
-        if get_value is not None:
-            area, category_mask = get_value
+        category_mask = mask_dict.get(category_name)
+        if category_mask is not None:
             category_mask = category_mask | mask
         else:
             category_mask = mask
         
-        mask_dict[category_name] = (category_mask.sum(), category_mask)
+        mask_dict[category_name] = category_mask
     
     # Create sorted list by mask area
-    category_list = sorted(mask_dict, key=lambda x: mask_dict[x][0], reverse=True)
+    category_list = sorted(mask_dict, key=lambda x: mask_dict[x].sum(), reverse=True)
     
     # Initialize visualize image
     vis_image = np.array(image)
@@ -200,15 +199,6 @@ for idx, i in enumerate(range(len(dataset))):
             + list(map(aggregate_llm_attention, outputs["attentions"]))
         )
 
-        # visualize the llm attention matrix
-        # ===> adjust the gamma factor to enhance the visualization
-        #      higer gamma brings out more low attention values
-        # gamma_factor = 1
-        # enhanced_attn_m = np.power(llm_attn_matrix.numpy(), 1 / gamma_factor)
-
-        # fig, ax = plt.subplots(figsize=(10, 20), dpi=150)
-        # ax.imshow(enhanced_attn_m, vmin=enhanced_attn_m.min(), vmax=enhanced_attn_m.max(), interpolation="nearest")
-
         # identify length or index of tokens
         input_token_len = model.get_vision_tower().num_patches + len(input_ids[0]) - 1 # -1 for the <image> token
         vision_token_start = len(tokenizer(prompt.split("<image>")[0], return_tensors='pt')["input_ids"][0])
@@ -225,29 +215,12 @@ for idx, i in enumerate(range(len(dataset))):
                 outputs["sequences"][0].tolist()
             )
         ):
-            # print(
-            #     i + input_token_len, 
-            #     f"{tokenizer.decode(token, add_special_tokens=False).strip():<15}", 
-            #     f"{row[vision_token_start:vision_token_end].sum().item():.4f}"
-            # )
-
             overall_attn_weights_over_vis_tokens.append(
                 row[vision_token_start:vision_token_end].sum().item()
             )
 
-        # plot the trend of attention weights over the vision tokens
-        # fig, ax = plt.subplots(figsize=(20, 5))
-        # ax.plot(overall_attn_weights_over_vis_tokens)
-        # ax.set_xticks(range(len(overall_attn_weights_over_vis_tokens)))
-        # ax.set_xticklabels(
-        #     [tokenizer.decode(token, add_special_tokens=False).strip() for token in outputs["sequences"][0].tolist()],
-        #     rotation=75
-        # )
-        # ax.set_title("at each token, the sum of attention weights over all the vision tokens")
-
-        # connect with the vision encoder attention
-        # to visualize the attention over the image
-
+        # Connect with the vision encoder attention
+        # to visualize the attention over the image.
         # vis_attn_matrix will be of torch.Size([N, N])
         # where N is the number of vision tokens/patches
         # `all_prev_layers=True` will average attention from all layers until the selected layer
@@ -259,24 +232,11 @@ for idx, i in enumerate(range(len(dataset))):
         )
         grid_size = model.get_vision_tower().num_patches_per_side
 
-        num_image_per_row = 8
-        image_ratio = image_size[0] / image_size[1]
-        num_rows = output_token_len // num_image_per_row + (1 if output_token_len % num_image_per_row != 0 else 0)
-        # fig, axes = plt.subplots(
-        #     num_rows, num_image_per_row, 
-        #     figsize=(10, (10 / num_image_per_row) * image_ratio * num_rows), 
-        #     dpi=150
-        # )
-        # plt.close()
-        # plt.subplots_adjust(wspace=0.05, hspace=0.2)
-
         # whether visualize the attention heatmap or 
         # the image with the attention heatmap overlayed
-        vis_overlayed_with_attn = True
 
         output_token_inds = list(range(output_token_start, output_token_end))
         heat_torch_stack = []
-        
         
         ####
         #### input / ouput swap 가능
@@ -312,19 +272,7 @@ for idx, i in enumerate(range(len(dataset))):
             # tt = tokenizer.decode(outputs["sequences"][0][i], add_special_tokens=False).strip()
             # tt = tokenizer.decode(input_ids[0][i], add_special_tokens=False).strip()
             img_with_attn = cv2.cvtColor(img_with_attn, cv2.COLOR_BGR2RGB)
-            # cv2.imwrite(f"./img/{str(i).zfill(4)}_{tt}.png", img_with_attn)
-            bp = "bp"
-            # ax.imshow(heatmap if not vis_overlayed_with_attn else img_with_attn)
-            # ax.set_title(
-            #     tokenizer.decode(outputs["sequences"][0][i], add_special_tokens=False).strip(),
-            #     fontsize=7,
-            #     pad=1
-            # )
-            # ax.axis("off")
 
-        # if "Yes" in text:
-        #     bp = "bp"
-        # elif "No" in text:
         os.makedirs(f"./img/{ret}_{idx}_{idx2}", exist_ok=True)
         med = torch.stack(heat_torch_stack, dim=0)
         med = med.mean(dim=0)
@@ -356,13 +304,10 @@ for idx, i in enumerate(range(len(dataset))):
             #     import pickle
             #     myd = {}
             #     temp = F.interpolate(attn.unsqueeze(0).unsqueeze(0),  (256, 256), mode="bilinear", align_corners=False).squeeze().numpy()
-            #     temp = temp + 0.2 # bias
-                
+            #     temp = temp + 0.2 # bias  
             #     temp[temp > 1 - 0.001] = 1 - 0.001
             #     temp[temp < 0.001] = 0.001
-                
             #     mask = (temp > 0.5).astype(np.uint8) * 255 # vis
-                
             #     temp = np.log(temp / (1 - temp)) # inv_sigmoid
             #     myd["heat"] = temp
             #     with open('my_dict.pkl', 'wb') as f:
@@ -391,6 +336,7 @@ for idx, i in enumerate(range(len(dataset))):
         h, w = mask.shape
         attn_h, attn_w = attn.size()
         mask1 = mask
+        
         if h != attn_h:
             half_h = (attn_h - h) // 2
             block1 = np.zeros((half_h, w), dtype=np.uint8)
@@ -401,6 +347,7 @@ for idx, i in enumerate(range(len(dataset))):
             block1 = np.zeros((h, half_w), dtype=np.uint8)
             block2 = np.zeros((h, attn_w - w - half_w), dtype=np.uint8)
             mask1 = cv2.hconcat([block1, mask1, block2])
+            
         gt = torch.tensor(mask1, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
         attn_unsq = attn.unsqueeze(0).unsqueeze(0)
         seg_loss = monai.losses.DiceCELoss(sigmoid=False, squared_pred=True, reduction='mean')
@@ -408,5 +355,6 @@ for idx, i in enumerate(range(len(dataset))):
         loss = seg_loss(gt, attn_unsq)
         l2 = l2_loss(gt, attn_unsq)
         print(loss.item(), l2.item())
+        
         with open(f"./log.csv", "a") as f:
             f.write(f"{ret},{idx},{idx2},{cat_name},{loss.item()},{l2.item()}\n")
